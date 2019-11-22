e132451cf14519d7ad11a35c24df20335e28c922
kodaikodai <k.sagawaa@gmail.com>
2019-11-22 12:35:30 +0900

    自動更新機能実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){
   function buildHTML(message){
-    var addImage = (message.image !== null) ? `<img class="ChatMain__messages__message__text__image" src=${message.image}>` : '' 
+    var addImage = (message.image !== null) ? `<img class="ChatMain__messages__message__text__image" src=${message.image}>` :"";
     var html =
     `<div class="ChatMain__messages__message" data-message-id=${message.id}>
       <div class="ChatMain__messages__message__upper-info">
@@ -8,7 +8,7 @@ $(function(){
           ${message.user_name}
         </p>
         <p class="ChatMain__messages__message__upper-info__date">
-          ${message.date}
+          ${message.created_at}
         </p>
       </div>
       <div class="ChatMain__messages__message__text">
@@ -22,8 +22,7 @@ $(function(){
     return html
   }
 
- 
-
+  // ここからインクリメンタルサーチの実装
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -46,5 +45,31 @@ $(function(){
       alert('error');
     });
     return false;
-  })
-})
+    });
+    
+    // ここから自動更新機能
+    
+    var reloadMessages = function() {
+      if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id =$('.ChatMain__messages__message').last().data('message-id');
+      $.ajax({
+        url: "api/messages",
+        type: 'GET',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        var insertHTML ='';
+        messages.forEach(function (message){
+          var insertHTML=buildHTML(message);
+          $('.ChatMain__messages').append(insertHTML);
+        })
+        $('.ChatMain__messages').animate({scrollTop: $('.ChatMain__messages')[0].scrollHeight}, 'fast');
+      })
+      .fail(function() {
+        alert('error');
+      });
+    }
+  }
+  setInterval(reloadMessages, 7000); 
+});
